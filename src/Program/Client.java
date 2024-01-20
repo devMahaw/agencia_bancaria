fa292f84ff118fc18933300013e0ba01d637ad16
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-01-20 11:47:59 -0300

    feat: client class made

--- src/Program/Client.java
+++ src/Program/Client.java
@@ -1,4 +1,47 @@
 package Program;
 
 public class Client {
+
+    private static long counter = 1;
+
+    private String name;
+    private String cpf;
+    private String email;
+
+    public Client(String name, String cpf, String email) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        counter += 1;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public java.lang.String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(java.lang.String cpf) {
+        this.cpf = cpf;
+    }
+
+    public java.lang.String getEmail() {
+        return email;
+    }
+
+    public void setEmail(java.lang.String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "\nName: " + this.getName() +
+               "\nCPF: " + this.getCpf() +
+               "\nE-mail: " + this.getEmail();
+    }
 }
